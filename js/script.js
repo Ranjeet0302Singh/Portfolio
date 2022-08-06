93c6cc0d7d619ba79d3adf6cd37dbfdc1d6b77dc
Ranjeet-Rawat <ranjeetsingh01266@gmail.com>
2022-08-06 17:54:24 +0530

    all ost done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//  typeing animation 
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","Graphirc Designer","Web Designer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+
+})
